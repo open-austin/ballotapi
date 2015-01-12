3cb5234e1fbb4f3898ac9fae6537d84fac68b461
Gregory Arenius <gregory@arenius.com>
2015-01-12 14:44:28 -0800

    Start work on measures endpoint.

--- ballotapi.py
+++ ballotapi.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, request
 import json
 import psycopg2
@@ -42,7 +43,8 @@ with psycopg2.connect(db_login_string) as conn:
                       'elections':None,
                       'measures':None,
                       'geo':None,
-                      'election_dates':None}
+                      'election_dates':None,
+                      'll':None}
         param_list = []
         #Initialize our SQL clauses.
         select_clause = ' SELECT DISTINCT p.precinct_id '
@@ -75,20 +77,54 @@ with psycopg2.connect(db_login_string) as conn:
                 from_clause += ', mappings M{} '.format(index)
                 where_clause += ' AND p.precinct_id = m{}.precinct_id '.format(index)
                 where_clause += ' AND m{}.measure_id IN %s '.format(index)
+                #split record by comma, then make each split piece and integer and
+                #make them all into a tuple of integers.
                 param_list.append(tuple(int(x) for x in item.split(',')))
         #?geo=
         if param_dict['geo']:
             geo_list = json.loads('[{}]'.format(param_dict['geo'][0]))
             geo_clause = ' OR ST_Intersects(p.geom, %s) '
-            where_clause += 'AND (ST_Intersects(p.geom, %s) {} ) '.format(geo_clause * \
+            where_clause += ' AND (ST_Intersects(p.geom, %s) {} ) '.format(geo_clause * \
                                                                         len(geo_list[1:]))
             param_list += geo_list
+        #?ll=
+        if param_dict['ll']:
+            coords = param_dict['ll'][0].split(',')
+            if len(coords) % 2 != 0:
+                return ('Error: You have entered an odd number of coordinates.  Coordinates '
+                        'must be entered in pairs')
+            if len(coords) = 2:
+                where_clause += ' AND ST_Intersects(p.geom, ST_MakePoint(%s,%s)) '
+            else:
+                collect_clause = ('ST_MakePoint(%s,%s),' * (len(coords)//2))[:-1]
+                where_clause += ' AND ST_Intersects(p.geom,ST_Collect({})) '.format(collect_clause)
+            for num in coords:
+                param_list.append(float(num))
         #?election_dates=
         if param_dict['election_dates']:
             from_clause += ', elections E '
-            where_clause += ' AND e.election_date BETWEEN %s AND %s '
-            param_list.append(x for x in param_dict['election_dates'][0].split(':'))
-        
+            where_clause += ' AND e.election_id = p.precinct_id '
+            single_dates = []
+            date_pairs = []
+            #Split date inputs into single_dates and date_pairs.
+            all_dates = param_dict['election_dates'][0].split(',')
+            for date in all_dates:
+                if ':' in date:
+                    date_pairs.append(date)
+                else:
+                    single_dates.append(date)
+            #Deal with single_dates.
+            if single_dates:
+                where_clause += ' AND e.election_date IN %s '
+                param_list.append(tuple(datetime.datetime.strptime(x, '%Y-%m-%d').date() for x in single_dates))
+            #Deal with date_pairs.
+            if date_pairs:
+                for date_pair in date_pairs:
+                    where_clause += ' AND e.election_date BETWEEN %s AND %s '
+                    #param_list.append(date_pair.split(':'))
+                    for x in date_pair.split(':'):
+                        param_list.append(datetime.datetime.strptime(x, '%Y-%m-%d').date())
+
         #Main Query.
         sql = select_clause + from_clause + where_clause + order_by_clause
         precinct_id_list = []
@@ -100,5 +136,40 @@ with psycopg2.connect(db_login_string) as conn:
         for id in precinct_id_list:
             precinct_data.append(build_precinct_data(id))
         return str(precinct_data)
+        """
+        with conn.cursor() as cur:            
+            return cur.mogrify(sql, param_list)
+        """
+
+    def build_measure_data(measure_id):
+        with conn.cursor() as cur:
+            measure_data = []
+            #Retrieve precinct data                                                             
+            sql = (' SELECT me.measure_id, me.election_id, me.info, me.title, '
+                   'me.question, me.type, me.voting_method, me.choices '
+                   ' FROM precincts P '
+                   ' WHERE me.measure_id = %s ')
+            cur.execute(sql, (measure_id,))
+            for record in cur:
+                for item in record:
+                    precinct_data.append(item)
+            #Retrieve precinct list
+            sql = (' SELECT ma.precinct_id '
+                   ' FROM mappings MA '
+                   ' WHERE ma.measure_id = %s ')
+            cur.execute(sql, (measure_id,))
+            precinct_list = []
+            for record in cur:
+                for item in record:
+                    measure_list.append(item)
+            measure_data.append(sorted(precinct_list))
+            return measure_data
+
+
+    @app.route('/measures/<int:measure_id>')
+    def pass_through_mid(measure_id):
+        return build_measure_data(measure_id)
+
+
 
 app.run(host='0.0.0.0', debug=True)
