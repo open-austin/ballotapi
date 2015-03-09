8ff1d7cb57ae8308b2e8d96a322e9a14742c929a
Gregory Arenius <gregory@arenius.com>
2015-03-09 16:27:23 -0700

    Add explicit .json endpoints.

--- source/ballotapi.py
+++ source/ballotapi.py
@@ -14,26 +14,32 @@ cor = CORS(app)
 #The app.route('/route/') defines which urls the following functions respond to.  Each function
 #can process url base queries at that endpoint and return JSON formatted data.
 @app.route('/api/precincts/<precinct_id>')
+@app.route('/api/precincts.json/<precinct_id>')
 def precinct_id(precinct_id):
     return formatter.precinct_json(precincts.id_endpoint(precinct_id))
     
 @app.route('/api/precincts/')
+@app.route('/api/precincts.json')
 def call_precincts():
     return formatter.precinct_json(precincts.endpoint(request.values))
 
 @app.route('/api/measures/<measure_id>')
+@app.route('/api/measures.json/<measure_id>')
 def measure_id(measure_id):
     return formatter.measure_json(measures.id_endpoint(measure_id))
 
 @app.route('/api/measures/')
+@app.route('/api/measures.json')
 def call_measures():
     return formatter.measure_json(measures.endpoint(request.values))
 
 @app.route('/api/elections/<election_id>')
+@app.route('/api/elections.json/<election_id>')
 def election_id(election_id):
     return formatter.election_json(elections.id_endpoint(election_id))
 
 @app.route('/api/elections/')
+@app.route('/api/elections.json')
 def call_elections():
     return formatter.election_json(elections.endpoint(request.values))
 
