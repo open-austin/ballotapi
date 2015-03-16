b023ce91a96c7668c7391460154286703b522927
Gregory Arenius <gregory@arenius.com>
2015-03-16 14:13:32 -0700

    Fix another small missing paren bug.

--- source/queries.py
+++ source/queries.py
@@ -158,11 +158,11 @@ def precincts_query(q_dict, param_dict):
 #Check for 'limit=' parameter or set query to default.
 def limit_query(qdict, param_dict):
     #Enforce that there is only one instance of 'limit=':
-    if len(param_dict['limit']) > 1):
+    if len(param_dict['limit']) > 1:
         message = ('Error: Only one instance of "limit=" is permitted')
         raise exception.BadRequestError(message)
     #Enforce that limit= only accepts one value.
-    if len(param_dict['limit'][0] > 1:
+    if len(param_dict['limit'][0]) > 1:
         message = ('Error: The limit parameter only accepts one value.')
         raise exception.BadRequestError(message)
     try:
